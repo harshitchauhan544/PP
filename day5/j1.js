a72fc2f69fb8861abe4f727311b7a3b08ec94c0d
harshitchauhan544 <harshitchauhan544@gmail.com>
2025-02-18 16:17:02 +0530

    day 5 updates

---
+++ day5/j1.js
@@ -0,0 +1,29 @@
+// Callbacks
+
+// Not callback
+
+// const sum = (a,b) => {
+//     const ans = a+b;
+//     return ans;
+// };
+
+// const printp = (txt) => {
+//     console.log("---------")
+//     console.log(txt)
+//     console.log("---------")
+// };
+
+
+// printp(sum(3,4));
+
+
+
+// Callback
+// passing a function as a parameter to another Function 
+
+// Higher order Functoion
+// It takes another function as an Argument 
+
+// One Should focus on de-coupling of code 
+// writing  modular code , such that parts can be used independently at multiple places
+
